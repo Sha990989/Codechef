148dd9f4e020a80bcc05960f0dc4003d4414941e
Sha990989 <anavaneethasankar@gmail.com>
2024-12-06 14:48:22 +0530

    Add files via upload

---
+++ foodcost.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class foodcost
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int x=sc.nextInt();
+	 int y=sc.nextInt();
+	 int totalcost=x*6+y;
+	 System.out.println(totalcost);
+
+	}
+}
