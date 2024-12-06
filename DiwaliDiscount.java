043bf679328950cd3fe41b92b6111b7b307d174e
Sha990989 <anavaneethasankar@gmail.com>
2024-12-06 13:42:31 +0530

    Add files via upload

---
+++ DiwaliDiscount.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class DiwaliDiscount
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=a-b;
+		System.out.println(c);
+	}
+}
