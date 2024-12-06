285ca6e2b8f56c27c5132f2a75188fa102643f64
Sha990989 <anavaneethasankar@gmail.com>
2024-12-06 11:09:37 +0530

    Add files via upload

---
+++ idesofmarch.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+ public class idesofmarch
+ { 
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n==15)
+		{
+		    System.out.println("Yes");
+		}
+		else{
+		    System.out.println("No");
+		}
+
+	}
+}
