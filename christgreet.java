683ca7a314e5c6a788877899d03924e62cf1f7a7
Sha990989 <anavaneethasankar@gmail.com>
2024-12-06 12:39:49 +0530

    Add files via upload

---
+++ christgreet.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class christgreet
+{
+	public static void main (String[] args) 
+	{
+	  Scanner sc=new Scanner(System.in);
+	  int d=sc.nextInt();
+	  if(d==25)
+	  System.out.println("CHRISTMAS");
+	  else
+	  System.out.println("Ordinary");
+
+	}
+}
+
