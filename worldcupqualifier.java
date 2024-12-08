7c46608160b06ba9f06fccff71073c3547076e04
Sha990989 <anavaneethasankar@gmail.com>
2024-12-08 21:30:49 +0530

    Add files via upload

---
+++ worldcupqualifier.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class worldcupqualifier
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n>=12)
+		{
+		    System.out.println("yes");
+		}
+		else{
+		    System.out.println("no");
+		}
+
+	}
+}
