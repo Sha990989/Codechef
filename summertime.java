bbdeee98cf6871d4ec0ea0956db4f03eeb38c219
Sha990989 <anavaneethasankar@gmail.com>
2024-12-08 21:30:30 +0530

    Add files via upload

---
+++ summertime.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class summertime
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		if(X>35)
+		System.out.println("YES");
+		else
+		System.out.println("NO");
+	}
+}
