aeb97a028c003ef9b4d2fde4ae2714631c7848b9
Sha990989 <anavaneethasankar@gmail.com>
2024-12-09 09:51:41 +0530

    Add files via upload

---
+++ glassprices.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class glassprices
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int X=sc.nextInt();
+	int Y=sc.nextInt();
+	if((2*X)>=Y)
+	System.out.println("METAL");
+	else
+	System.out.println("PLASTIC");
+
+	}
+}
